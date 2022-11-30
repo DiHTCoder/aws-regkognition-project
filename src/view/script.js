9ea09a6abc44db8d0243574af5d0c54b1af281e5
dihuynh@outlook.sg <dihuynh@outlook.sg>
2022-11-30 11:08:43 +0700

    delete folder

--- src/view/script.js
+++
@@ -1,7 +0,0 @@
-var fileUpload;
-
-document.getElementById("imageFace").addEventListener("change", (e) => {
-  fileImage = e.target.files[0];
-  const src = URL.createObjectURL(fileImage);
-  document.getElementById("image").src = src;
-});
