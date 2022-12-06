761218d45124a0014710b103d5e9c504e7a70d76
duynn01092002 <nguyenngocduy12a2@gmail.com>
2022-12-06 22:56:36 +0700

    add script

--- view/script.js
+++ view/script.js
@@ -15,6 +15,7 @@ document.getElementById("imageUp").addEventListener("change", (e) => {
 document.getElementById("btnDetectFace").addEventListener("click", (e) => {
     e.preventDefault();
 
+    
     axios.post("/detectFace", { name: fileName }).then((response) => {
         const result = response.data.data.FaceDetails;
         console.log(result);
@@ -25,7 +26,34 @@ document.getElementById("btnDetectText").addEventListener("click", (e) => {
     e.preventDefault();
 
     axios.post("/detectText", { name: fileName }).then((response) => {
-        const result = response.data.data.TextDetections;
-        console.log(result);
+        var result = response.data.data.TextDetections;
+        var data = document.getElementByClassName("data");
+        var boundingBox = document.getElementById("image-display");
+        data.innerHTML = "";
+        for (let i = 0; i < result.length; i++) {
+            var box = result[i].Geometry.BoundingBox;
+            var image = document.getElementById("imageDisplay");
+            data.innerHTML += `<span> DetectText: ${result[i].DetectedText} </span><br>
+                                <span> Type: ${result[i].Type == "WORD" ? "Từ" : "Dòng"}</span>`
+            data.innerHTML += `<br><br>`
+            boundingBox.innerHTML += `<div class="bounding-box" style="display: block;
+                                                            height:${
+                                                                box.Height *
+                                                                image.height
+                                                            }px; 
+                                                            width: ${
+                                                                box.Width *
+                                                                image.width
+                                                            }px;
+                                                            top: ${
+                                                                box.Top *
+                                                                image.height
+                                                            }px; 
+                                                            left: ${
+                                                                box.Left *
+                                                                image.width
+                                                            }px;
+                                                            border: 2px solid green"> </div> `;
+        }
     });
 });
