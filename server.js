4d3155d1a2427c85633b01f53e65186acc20013f
duynn01092002 <nguyenngocduy12a2@gmail.com>
2022-12-15 10:48:33 +0700

    edit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { S3Client } = require("@aws-sdk/client-s3");
+const { S3Client } = require("@aws-sdk/clients/s3");
 const bodyParser = require("body-parser");
 const multerS3 = require("multer-s3");
 const multer = require("multer");
